7a815f60f749caf7d32b669349f86a2e7a5ac706
prateek sharma <patrik706@gmail.com>
2017-02-22 22:38:49 +0530

    Initial Commit

---
+++ chatApp/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  chatApp
+//
+//  Created by prateek sharma on 22/02/17.
+//  Copyright © 2017 prateek sharma. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
